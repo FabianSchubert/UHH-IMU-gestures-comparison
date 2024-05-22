07540e725f167aa8c470b6f0a4c2a793a210ab99
FabianSchubert <fj.schubert@freenet.de>
2024-05-22 10:50:17 +0100

    .

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='UHH IMU gestures',
+    version='1.0.0',
+    description='A package providing IMU gesture data and tools for working with it. This is a fork of https://github.com/swtietz/UHH-IMU-gestures-comparison',
+    author='Fabian Schubert',
+    author_email='your@email.com',
+    url='https://github.com/FabianSchubert/UHH-IMU-gestures-comparison',
+    packages=find_packages(),
+    install_requires=[
+        # Add any dependencies here
+    ],
+)
